2215a55371a247e6c4421ab5fa21e262da73e0cc
unknown <panna1997@yandex.ru>
2018-04-03 17:11:15 +0300

    Save before merge

--- src/Application.java
+++ src/Application.java
@@ -1,9 +1,23 @@
+import dao.ClassDao;
+import dao.DaoFactory;
+import dao.Interfaces.IClassDao;
+import model.Class;
+
+import java.sql.Connection;
+
 /**
  * Created by PANNA on 02.04.2018.
  */
 public class Application {
     public static void main(String[] args) {
 //        System.out.println(PaidSalaryService.getInstance().findByEmployeeId(3));
+        IClassDao classDao = DaoFactory.getInstance().createClassDao();
+        Class classEx = new Class();
+        classEx.setCapacity(1);
+        classEx.setBuilding(1);
+        classEx.setNumber(2);
+        System.out.println(classEx.toString());
+        classDao.create(classEx);
 
     }
 }
