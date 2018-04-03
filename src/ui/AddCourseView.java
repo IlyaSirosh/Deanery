36771370a6ef9cd085df518cb6d285f3108a81c9
Julia <arikamedoshika@gmail.com>
2018-04-03 13:54:42 +0300

    All courses view

--- src/ui/AddCourseView.java
+++ src/ui/AddCourseView.java
@@ -3,6 +3,9 @@ package ui;
 import controllers.decorators.RequestPath;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.WindowEvent;
+import java.util.HashMap;
 import java.util.Map;
 
 @RequestPath("/addCourse")
@@ -10,19 +13,20 @@ public class AddCourseView extends View{
 
     @Override
     public void renderView() {
-<<<<<<< HEAD
         JFrame f = new JFrame();
         f.setSize(400, 200);
+        f.setLayout(new BorderLayout());
         JTextField textField = new JTextField("This is a text");
-        f.add(textField);
+        JButton b = new JButton("GOGOGOGO");
+        f.add(b, BorderLayout.SOUTH);
+        b.addActionListener(e -> {
+            HashMap<String, Object> params = new HashMap<>();
+            params.put("testValue", textField.getText());
+            handleSubmit("/try", params);
+            f.dispatchEvent(new WindowEvent(f, WindowEvent.WINDOW_CLOSING));
+        });
+        f.add(textField, BorderLayout.NORTH);
         f.setVisible(true);
-=======
-        System.out.println("Trying to render view");
->>>>>>> Oleksii
-    }
-
-    @Override
-    public void handleSubmit(String path, Map<String, Object> params) {
 
     }
 
